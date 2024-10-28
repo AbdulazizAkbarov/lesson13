af823547f5110099b28b61f3ba938764cad1f8db
Abdulaziz <abdulazizakbarov065@gmail.com>
2024-10-28 20:03:07 +0500

    Salom

---
+++ app.js
@@ -0,0 +1,123 @@
+let data = [
+    {
+      id:1,
+      name: "John Doe",
+      age: 30,
+      email: "john@example.com",
+      phone: "123-456-7890",
+    },
+    {
+      id:2,
+      name: "Jane Smith",
+      age: 25,
+      email: "jane@example.com",
+      phone: "987-654-3210",
+    },
+    {
+      id:3,
+      name: "Tom Hardy",
+      age: 40,
+      email: "tom@example.com",
+      phone: "765-432-1098",
+    },
+    {
+      id:4,
+      name: "Emily Blunt",
+      age: 28,
+      email: "emily@example.com",
+      phone: "321-654-9870",
+    },
+    {
+      id:5,
+      name: "Emily Blunt",
+      age: 28,
+      email: "emily@example.com",
+      phone: "321-654-9870",
+    },
+  ];
+
+//Shart-1//
+
+
+//   function fn(news) {
+  
+//   data.push(news)
+
+//   }
+
+//   fn({
+//     id :6,
+//     name:"Abdulaziz Akbarov",
+//     age: 18,
+//     email: "abdulazizakbarov065@gmail.com",
+//     phone:"93-823-1*-7*"
+
+//   }
+
+//   );
+//   console.log(data);
+
+
+  //Shart-1 Tugashi//
+
+
+  //Shart-3//
+
+//   function Update(id, updarteData){
+//   let index = data.findIndex((val)=>
+//   val.id= id)
+//   if(index== -1) return 
+//   data[index]={
+//     ...data[index],
+//     ...updarteData,
+//   };
+//     return data
+// }
+//     console.log(Update(1, {name:"Abdulaziz Akbarov"}));
+
+
+
+//Shart-3 Tugashi//
+
+  
+//Shart-4//
+
+// function deleteo(id){
+//   let filter =data.filter((val)=>val.id !==id) 
+//   return filter
+// }
+// console.log(deleteo(1))
+
+
+//Shart-4///
+
+//shart-5//
+
+// update(2, {
+
+//   name: "Jane Doe",
+//   age: 26,
+//   email: "jane.doe@example.com",
+
+// });
+
+// console.log(data);
+
+// function update(id, updateData) {
+//   let index = data.findIndex((val) => val.id == id);
+//   if (index == -1) return "user not found";
+
+//   data[index] = {
+
+//     ...data[index],
+//     ...updateData,
+
+//   };
+
+//   return data
+// }
+
+// console.log(update(3, {name:"Abdulaziz AKbarov"}));
+
+
+//Shart-5 Tugashi//
